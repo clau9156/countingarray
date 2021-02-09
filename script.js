e96a9765c9476f5a8562007ac9831ff59d68ab5a
clau9156 <clau9156@stud.kea.dk>
2021-02-09 21:33:47 +0100

    skeleton for html and steps for javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+// counts in the console
+// new line every count
+
+// every second "counter" increments(++) by 1
+
+// "counter" then added to the start of an array
+
+// array is "truncated" never becomes longer than 9
+
+// array logged to the console
+
+// DESIGN
+// two variables: the counter & the array
+// only one should be "let" variable
+
+// one function
+function something() {
+    // make a progtam that counts (with time delay)
+
+    // <--- modify the program to insert the counter-value in the beginning of the array
+
+    // <--- modify the program again, to make sure the array doesnt get longer than nine items.
+}
